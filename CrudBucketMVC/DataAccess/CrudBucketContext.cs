1bff3100260c1a406424daad907144589801e2b2
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-07-26 14:14:01 -0600

    Add project files.

---
+++ CrudBucketMVC/DataAccess/CrudBucketContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudBucketMVC.DataAccess
+{
+    public class CrudBucketContext : DbContext
+    {
+        public CrudBucketContext(DbContextOptions<CrudBucketContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ CrudBucketMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudBucketMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
